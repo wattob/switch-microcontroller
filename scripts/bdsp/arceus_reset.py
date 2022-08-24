7a2fbd91811b7af1ace33227bcc76a9a556793fd
wattob <wattobd@gmail.com>
2022-08-24 16:56:53 -0400

    adding email alert and troubleshooting changes

--- scripts/bdsp/arceus_reset.py
+++ scripts/bdsp/arceus_reset.py
@@ -10,8 +10,43 @@ import cv2
 import numpy
 import serial
 
-SERIAL_DEFAULT = 'COM1' if sys.platform == 'win32' else '/dev/ttyUSB0'
+import smtplib
+import ssl
+from email.message import EmailMessage
 
+from dotenv import load_dotenv
+import os
+ 
+# Use load_env to trace the path of .env
+load_dotenv('../../.env') 
+
+# find serial bus controller in Device Manager for COM Ports on your devices
+SERIAL_DEFAULT = 'COM3' if sys.platform == 'win32' else '/dev/ttyUSB0'
+
+def sendEmail(count):
+    # Define email sender and receiver
+    # Get the values of the variables from .env using the os library
+    email_sender = os.environ.get("email_sender")
+    email_password = os.environ.get("email_password")
+    email_receiver = os.environ.get("email_receiver")
+
+    # Set the subject and body of the email
+    subject = 'Check out the shiny encounter!'
+    body = f"""Currently shiny hunting Arceus! Finally encountered a shiny at {count} resets!"""
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    # Add SSL (layer of security)
+    context = ssl.create_default_context()
+
+    # Log in and send the email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 def _press(ser: serial.Serial, s: str, duration: float = .1) -> None:
     print(f'{s=} {duration=}')
@@ -26,6 +61,21 @@ def _getframe(vid: cv2.VideoCapture) -> numpy.ndarray:
     cv2.imshow('game', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         raise SystemExit(0)
+    # will show RGB and X and Y coords for troubleshooting
+    def mouse_pos_BGR(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN: # Left mouse click
+            
+            B = frame[y,x,0]
+            G = frame[y,x,1]
+            R = frame[y,x,2]
+        
+            print("Blue: ", B)
+            print("Green: ", G)
+            print("Red: ", R)        
+
+            print("Coords x: ", x," y: ", y)
+    cv2.namedWindow('game')
+    cv2.setMouseCallback('game',mouse_pos_BGR)
     return frame
 
 
@@ -81,7 +131,7 @@ def _color_near(pixel: numpy.ndarray, expected: tuple[int, int, int]) -> bool:
     total = 0
     for c1, c2 in zip(pixel, expected):
         total += (c2 - c1) * (c2 - c1)
-
+        
     return total < 76
 
 
@@ -101,26 +151,24 @@ def main() -> int:
     vid = cv2.VideoCapture(0)
     vid.set(cv2.CAP_PROP_FRAME_WIDTH, 768)
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    i = 0 # running number for the count of resets
 
     with serial.Serial(args.serial, 9600) as ser, _shh(ser):
         while True:
+            i = i + 1
+            print(' total count ', i)
             _press(ser, 'H')
             _wait_and_render(vid, 1)
             _press(ser, 'X')
             _wait_and_render(vid, 1)
             _press(ser, 'A')
-            # TODO: we could notice the dialog quicker here
             _wait_and_render(vid, 3.5)
             _press(ser, 'A')
             _wait_and_render(vid, 1)
             _press(ser, 'A')
-
-            _await_pixel(ser, vid, x=5, y=5, pixel=(16, 16, 16))
-
-            print('startup screen!')
-
+            print('Loading screen!')
             frame = _getframe(vid)
-            while not _color_near(frame[25][423], (227, 99, 50)):
+            while not _color_near(frame[167][500], (255, 162, 107)):
                 _wait_and_render(vid, .15)
                 _press(ser, 'A')
                 frame = _getframe(vid)
@@ -131,8 +179,8 @@ def main() -> int:
 
             frame = _getframe(vid)
             while not (
-                    _color_near(frame[420][330], (230, 230, 230)) and
-                    _color_near(frame[30][100], (144, 34, 34))
+                    _color_near(frame[900][900], (254, 254, 254)) and
+                    _color_near(frame[44][236], (157, 29, 20)) # frame[y][x], (...)
             ):
                 _wait_and_render(vid, .1)
                 frame = _getframe(vid)
@@ -141,20 +189,21 @@ def main() -> int:
             _press(ser, 'A')
             _wait_and_render(vid, 1)
 
-            _await_pixel(ser, vid, x=330, y=420, pixel=(230, 230, 230))
+            _await_pixel(ser, vid, x=900, y=900, pixel=(254, 254, 254))
             print('dialog started')
-            _await_not_pixel(ser, vid, x=330, y=420, pixel=(230, 230, 230))
+            _await_not_pixel(ser, vid, x=900, y=900, pixel=(254, 254, 254))
 
             print('dialog ended')
             t0 = time.time()
 
-            _await_pixel(ser, vid, x=330, y=420, pixel=(230, 230, 230))
+            _await_pixel(ser, vid, x=900, y=900, pixel=(254, 254, 254))
 
             t1 = time.time()
             print(f'dialog delay: {t1 - t0:.3f}s')
 
             if (t1 - t0) > 1:
                 print('SHINY!!!')
+                sendEmail(i)
                 _alarm(ser, vid)
 
     vid.release()
